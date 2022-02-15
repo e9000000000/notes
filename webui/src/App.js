f52c3af3f42d1766575579d626ee25a903f0fb7a
6 <skipper224483@gmail.com>
2022-02-15 05:13:38 +0300

    added something like header

--- webui/src/App.js
+++ webui/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
-  return (
-	  <h1>hello web</h1>
-  );
+	return (
+		<div>
+			<Header />
+			<h1>hello web</h1>
+		</div>
+	);
 }
 
 export default App;
---
+++ webui/src/Header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+	return (
+		<header>
+			<h1>Notes</h1>
+			<div>
+				<p>login</p>
+			</div>
+		</header>
+	);
+}
