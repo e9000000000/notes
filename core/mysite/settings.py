6fdb8d44e3e5c54560b65ed534c18197d98b354d
6 <skipper224483@gmail.com>
2022-02-16 04:26:22 +0300

    add cookie token authentication

--- core/mysite/settings.py
+++ core/mysite/settings.py
@@ -124,6 +124,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
+        "users.authentication.CookieTokenAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
